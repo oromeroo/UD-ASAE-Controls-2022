2ebda7ede10a46d45ba3a80480973c7f099c8ca2
christophercalandra <112659186+christophercalandra@users.noreply.github.com>
2022-09-22 14:09:49 -0400

    Add files via upload

---
+++ servo_test.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo servoP; 
+
+void setup() {
+  Serial.begin(115200);
+  
+  servoP.attach(13);
+  servoP.write(0);
+  Serial.println("setup complete");
+  delay(8000);
+}
+
+void loop() {
+  servoP.write(0);
+  delay(1000);
+  servoP.write(180);
+  delay(1000);
+  Serial.println("spin");
+}
