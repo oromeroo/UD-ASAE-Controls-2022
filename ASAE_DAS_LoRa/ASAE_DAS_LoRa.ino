a4a7cb2d9ac343461bfac58a3470caac49cc2c0d
christophercalandra <112659186+christophercalandra@users.noreply.github.com>
2022-09-24 00:59:48 -0400

    Created outline for DAS_LoRa code

---
+++ ASAE_DAS_LoRa/ASAE_DAS_LoRa.ino
@@ -0,0 +1,46 @@
+/* 
+ *  ASAE 2022-2023 Controls Team "main plane" On-board DAS
+ *  Team: Evan Eng (Captain), Chris Calandra, Omar Romero, Andrew
+ *  Github for 2022-2023: https://github.com/christophercalandra/UD-ASAE-Controls-2022
+ *  Github fot 2021-2022: https://github.com/fdoyle6/UD-ASAE-Controls-2021
+ *  Github for 2020-2021: https://github.com/FirkinTage/UDASAE_Controls
+ *  The DAS code from previous years was referenced for early iterations of design
+ */
+
+//Libraries------------------------------------------------------------------------------------------------------------
+
+//Global Variables-----------------------------------------------------------------------------------------------------
+
+
+//LoRa
+//Altimeter 
+//IMU 
+//GPS 
+//PADA Servo
+//LEDS
+
+
+//Data Collection Functions--------------------------------------------------------------------------------------------
+
+//Visual data collection 
+//Altimeter data collection 
+//IMU data collection
+//GPS data collection
+//Packaging data 
+//LoRa Transmission
+
+//Setup----------------------------------------------------------------------------------------------------------------
+void setup() {
+  //GPS setup 
+  //Servo setup 
+  //LED setup 
+  //LoRa setup 
+  //IMU setup 
+  //Altimeter setup 
+}
+
+//Main Loop------------------------------------------------------------------------------------------------------------
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
